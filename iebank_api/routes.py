6741310bc79e5ff3da1f524e0f1e6ddddbe37548
cristina requena <crequena.ieu2021@student.ie.edu>
2023-10-13 18:21:03 +0200

    cvbn

--- iebank_api/routes.py
+++ iebank_api/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from iebank_api import db, app
 from iebank_api.models import Account
+import sys 
+sys.path.append('/Github/ie-backend/iebank_api')
 
 @app.route('/')
 def hello_world():
@@ -9,24 +11,15 @@ def hello_world():
 @app.route('/skull', methods=['GET'])
 def skull():
     text = 'Hi! This is the BACKEND SKULL! 💀 '
-    text = text +'<br/>Database URL:' + db.engine.url.database
-    if db.engine.url.host:
-        text = text +'<br/>Database host:' + db.engine.url.host
-    if db.engine.url.port:
-        text = text +'<br/>Database port:' + db.engine.url.port
-    if db.engine.url.username:
-        text = text +'<br/>Database user:' + db.engine.url.username
-    if db.engine.url.password:
-        text = text +'<br/>Database password:' + db.engine.url.password
     return text
 
 
 @app.route('/accounts', methods=['POST'])
 def create_account():
     name = request.json['name']
-    currency = request.json['currency']
     country = request.json['country']
-    account = Account(name, currency, country)
+    currency = request.json['currency']
+    account = Account(name, country, currency)
     db.session.add(account)
     db.session.commit()
     return format_account(account)
